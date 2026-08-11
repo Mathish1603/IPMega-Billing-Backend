c191d872cb8d07da3edd30f9e0f8388550f397fc
Mathish1603 <mathish1603@gmail.com>
2026-08-11 11:57:36 +0530

    link fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors({
   origin: ['http://localhost:4200',
-    'https://your-frontend.vercel.app'], credentials: true
+    'https://ip-mega-billing-frontend.vercel.app'], credentials: true
 }));
 app.use(express.json());
 
